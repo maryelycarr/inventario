8a7bd01a7a884bbf14aebb39dfafbf18a0a4c25a
Maryely Carrero <maryelycarrero@uts.edu.co>
2025-11-25 22:24:02 -0500

    connecting DB-http.get

--- src/controllers/categorias.controllers.js
+++ src/controllers/categorias.controllers.js
@@ -1,5 +1,14 @@
-const getCategorias = (req, res)=>{
-    res.json({"categoria":"Electrodomesticos"})
+import getconnection from "../db/database.js"
+const getCategorias = async (req, res)=>{ 
+
+    try {
+         const connection = await getconnection ();
+   const result = await connection.query("select categoriaID, categoriaNombre, Descripcion, Imagen FROM categorias")
+   res.json(result);
+    } catch (error) {
+        console.error("error");
+    }
+   
 }
 
 export const methodHTTP ={
